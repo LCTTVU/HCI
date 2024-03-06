875bead19e80aa6f82ac1b47d0ab0b7f7b083530
LCTTVU <lecungtuantranh@student.vu.nl>
2024-03-06 15:58:32 +0100

    routing & cleanup

--- main.js
+++ main.js
@@ -12,25 +12,19 @@ L.tileLayer('https://tile.jawg.io/d8548ead-a5fa-44e4-a37d-06b2d8e85a5a/{z}/{x}/{
 // Function to determine style based on feature properties
 function getBuildingStyle(feature,zoom) {
   var properties = feature.properties;
-  var fill;
-  var border;
-  var weight = 1;
-  var opacity = 1;
+  var fill = 'none';
+  var border = 'none';
+  var weight = 0;
+  var opacity = 0;
 
-  var id_list = ['4726588','4328975','2614014','16322055','4661214','868885682','1609614','75658','549109296','1006252510']
-  if (id_list.includes(properties.osm_id) || id_list.includes(properties.osm_way_id)) {
+  var special_id_list = ['4726588','4328975','2614014','16322055','4661214','868885682','1609614','75658','549109296','1006252510']
+  if (special_id_list.includes(properties.osm_id) || special_id_list.includes(properties.osm_way_id)) {
     fill = '#f17170';
     border = 'red';
   }
    else if (properties.place) {
     fill = '#60ca8c';
     border = 'green';
-
-  } else if (properties.leisure) {
-    fill = 'none';
-    border = 'none';
-    weight = 0;
-    opacity = 0;
   } else if (properties.historic) {
     fill = '#f17170';
     border = 'red'; 
@@ -79,27 +73,17 @@ function getBuildingStyle(feature,zoom) {
       case 'library':
         fill = '#a889c2';
         border = 'purple';
-        break;
-      default:
-        fill = 'none';
-        border = 'none';
-        weight = 0;
-        opacity = 0;
-        break;
-      
+        break;      
     }
   } else if (properties.shop == 'bakery' || properties.building == 'apartments') {
     fill = '#fa9b5c';
   }  
   else if (properties.building || properties.shop) {
     fill = '#ffdbc5';
-  } 
-  else {
-    fill = 'none';
-    border = 'none';
-    weight = 0;
-    opacity = 0;
   }
+
+  weight = 1;
+  opacity = 1;
 
   if (fill == '#fa9b5c' || fill == '#ffdbc5') {
     if (zoom < 16) {
@@ -116,57 +100,33 @@ function getBuildingStyle(feature,zoom) {
 var currentZoom = map.getZoom();
 console.log(currentZoom)
 
-// Define a custom icon with the desired color
-// Define a custom icon with the desired color (blue)
-// var greenIcon = L.icon({
-//     iconUrl: 'palacio.png',
+var icons = {
+  'palacio': [[40.418, -3.7142],'icons/palacio.png', [129, 82], [64.5, 82]],
+  'catedral': [[40.4157, -3.7144],'icons/catedral.png', [96, 100], [48, 100]],
+  'plazaEspana': [[40.4234, -3.7122],'icons/plaza_de_espana.png', [100, 85], [50, 85]],
+  'temploDebod': [[40.4240, -3.7177],'icons/templo_de_debod.png', [114, 82], [57, 82]],
+  'puertaSol': [[40.4170, -3.7034],'icons/puerta_de_sol.png', [84, 78], [42, 78]],
+  'plazaMayor': [[40.4154, -3.7073],'icons/plaza_mayor.png', [80, 78], [40, 78]],
+  'bancoEspana': [[40.418, -3.6948],'icons/banco_de_espana.png', [110, 82], [55, 82]],
+  'metropolis': [[40.4188, -3.6974],'icons/edificio_metropolis.png', [110, 78], [55, 78]],
+  'palacioCibeles': [[40.4184, -3.6918],'icons/palacio_de_cibeles.png', [110, 80], [55, 80]],
+  'fuenteCibeles': [[40.4193, -3.69305],'icons/fuente_de_cibeles.png', [110, 82], [55, 82]],
+  'fuenteNeptuno': [[40.4153, -3.6941],'icons/fuente_de_neptuno.png', [118, 82], [59, 82]],
+  'museoNacional': [[40.4137, -3.6922],'icons/museo_nacional.png', [100, 96], [50, 96]],
+  'puertaAlcala': [[40.420, -3.68875],'icons/puerta_de_alcala.png', [114, 82], [57, 82]]
+};
 
-//     iconSize:     [41, 51], // size of the icon
-    
-// });
+function addIcon(coordinates,url,size,anchor) {
+  var icon = L.icon({iconUrl: url,iconSize: size,iconAnchor: anchor});
+  L.marker(coordinates, {icon: icon}).addTo(map);
+}
 
-// // Create a marker using the custom icon
-// L.marker([40.418, -3.7142], {icon: greenIcon}).addTo(map);
-
-// Define a custom icon with text
-var palacioIcon = L.icon({iconUrl: 'icons/palacio.png',iconSize: [129,82],iconAnchor: [64.5,82]});
-
-var catedralIcon = L.icon({iconUrl: 'icons/catedral.png',iconSize: [96,100],iconAnchor: [48,100]});
-var plazaEspanaIcon = L.icon({iconUrl: 'icons/plaza_de_espana.png',iconSize: [100,85],iconAnchor: [50,85]});
-var temploDebodIcon = L.icon({iconUrl: 'icons/templo_de_debod.png',iconSize: [114,82],iconAnchor: [57,82]});
-var puertaSolIcon = L.icon({iconUrl: 'icons/puerta_de_sol.png',iconSize: [84,78],iconAnchor: [42,78]});
-var plazaMayorIcon = L.icon({iconUrl: 'icons/plaza_mayor.png',iconSize: [80,78],iconAnchor: [40,78]});
-var bancoEspanaIcon = L.icon({iconUrl: 'icons/banco_de_espana.png',iconSize: [110,82],iconAnchor: [55,82]});
-var metropolisIcon = L.icon({iconUrl: 'icons/edificio_metropolis.png',iconSize: [110,78],iconAnchor: [55,78]});
-var palacioCibelesIcon = L.icon({iconUrl: 'icons/palacio_de_cibeles.png',iconSize: [110,80],iconAnchor: [55,80]});
-var fuenteCibelesIcon = L.icon({iconUrl: 'icons/fuente_de_cibeles.png',iconSize: [110,82],iconAnchor: [55,82]});
-var fuenteNeptunoIcon = L.icon({iconUrl: 'icons/fuente_de_neptuno.png',iconSize: [118,82],iconAnchor: [59,82]});
-var museoNacionalIcon = L.icon({iconUrl: 'icons/museo_nacional.png',iconSize: [100,96],iconAnchor: [50,96]});
-var puertaAlcalaIcon = L.icon({iconUrl: 'icons/puerta_de_alcala.png',iconSize: [114,82],iconAnchor: [57,82]});
-
-
-
-L.marker([40.418, -3.7142], {icon: palacioIcon}).addTo(map);
-L.marker([40.4157, -3.7144], {icon: catedralIcon}).addTo(map);
-L.marker([40.4234, -3.7122], {icon: plazaEspanaIcon}).addTo(map);
-L.marker([40.4240, -3.7177], {icon: temploDebodIcon}).addTo(map);
-L.marker([40.4170, -3.7034], {icon: puertaSolIcon}).addTo(map);
-L.marker([40.4154, -3.7073], {icon: plazaMayorIcon}).addTo(map);
-L.marker([40.4188, -3.6974], {icon: metropolisIcon}).addTo(map);
-L.marker([40.418, -3.6948], {icon: bancoEspanaIcon}).addTo(map);
-
-L.marker([40.4184, -3.6918], {icon: palacioCibelesIcon}).addTo(map);
-L.marker([40.4193, -3.69305], {icon: fuenteCibelesIcon}).addTo(map);
-L.marker([40.4153, -3.6941], {icon: fuenteNeptunoIcon}).addTo(map);
-L.marker([40.4137, -3.6922], {icon: museoNacionalIcon}).addTo(map);
-L.marker([40.420, -3.68875], {icon: puertaAlcalaIcon}).addTo(map);
-
-
-
-
+for (var key in icons) {
+  addIcon(icons[key][0], icons[key][1], icons[key][2], icons[key][3]);
+}
 
 // Load GeoJSON data for buildings
-fetch('buildings.geojson')
+fetch('B.geojson')
     .then(response => response.json())
     .then(data => {
         // Create a layer for buildings
@@ -191,4 +151,12 @@ fetch('buildings.geojson')
               return getBuildingStyle(feature, currentZoom);
             });
         });
+
+        var control = L.Routing.control({
+          waypoints: [
+              L.latLng(40.418, -3.7142), // Start point
+              L.latLng(40.4184, -3.6918)    // End point
+          ],
+          routeWhileDragging: true // Show the route while dragging the waypoints
+      }).addTo(map);
     });
